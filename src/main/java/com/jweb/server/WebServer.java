fb37227728b40ace471ea15eef85f1a405e7cdf6
JonAyre <jon@aacl.co.uk>
2024-08-05 16:16:28 +0100

    Added input argument handling for main method (args are -ip and -p for ip address and port of server. Defaults are 0.0.0.0 and 8080).
    Moved add item page to additem.html and created new index.html welcome page.
    
    Added a jquery example.html

--- src/main/java/com/jweb/server/WebServer.java
+++ src/main/java/com/jweb/server/WebServer.java
@@ -2,6 +2,7 @@ package com.jweb.server;
 
 import com.jweb.todolist.ToDoHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.cli.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -10,8 +11,39 @@ public class WebServer {
     // Main Method
     public static void main(String[] args) throws IOException
     {
-        // Create an HttpServer instance
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        Options options = new Options();
+
+        Option input = new Option("ip", "ipaddress", true, "ip address of this server");
+        input.setRequired(false);
+        options.addOption(input);
+
+        Option output = new Option("p", "port", true, "port on which the server will listen");
+        output.setRequired(false);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;//not a good practice, it serves it purpose
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
+
+        String ip = cmd.getOptionValue("ipaddress");
+        String portStr = cmd.getOptionValue("port");
+        int port;
+
+        if (ip == null) ip = "0.0.0.0";
+        if (portStr == null)
+            port = 8080;
+        else
+            port = Integer.parseInt(portStr);
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
 
         // Create a context for a specific path and set the handler
         server.createContext("/todolist", new ToDoHandler());
@@ -22,7 +54,7 @@ public class WebServer {
         server.setExecutor(null); // Use the default executor
         server.start();
 
-        System.out.println("Server is running on port 8000");
+        System.out.println("Server is running on " + ip + ":" + port);
     }
 
 }
