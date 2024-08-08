42073a19f5900178974624d4d6ca9bbb8debf5d7
JonAyre <jon@aacl.co.uk>
2024-08-08 16:38:17 +0100

    Moved creation of Toast element to jweb.js

--- web/js/jweb.js
+++ web/js/jweb.js
@@ -6,16 +6,21 @@ function showToast(id)
 
 function makeToast(containerId, toastId, title, message)
 {
-    let container = $('#' + containerId);
-    container.innerHTML =
-        '<div id="' + toastId + '" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="3000">\n' +
-        '    <div class="toast-header">\n' +
-        '        <strong class="me-auto">' + title + '</strong>\n' +
-        '        <button type="button" class="btn-close" data-bs-dismiss="toast"></button>\n' +
-        '    </div>\n' +
-        '    <div class="toast-body text-muted">\n' +
-        '        <h2>' + message + '</h2>\n' +
-        '    </div>\n' +
+    let container = document.getElementById(containerId);
+    let toast = document.createElement('div');
+    toast.className = 'toast hide';
+    toast.id = toastId;
+    toast.setAttribute('role', 'alert');
+    toast.setAttribute('aria-live', 'assertive');
+    toast.setAttribute('aria-atomic', 'true');
+    toast.setAttribute('data-bs-delay', '3000');
+    toast.innerHTML =
+        '<div class="toast-header">\n' +
+        '    <strong class="me-auto">' + title + '</strong>\n' +
+        '    <button type="button" class="btn-close" data-bs-dismiss="toast"></button>\n' +
+        '</div>\n' +
+        '<div class="toast-body text-muted">\n' +
+        '    <h2>' + message + '</h2>\n' +
         '</div>\n';
-    alert(container.innerHTML);
+    container.appendChild(toast);
 }
