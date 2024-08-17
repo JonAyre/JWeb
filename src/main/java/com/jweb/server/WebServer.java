4fdc5e5a83ca4ffa781c5b4a692882129859c7ef
JonAyre <jon@aacl.co.uk>
2024-08-17 09:35:48 +0100

    Extracted functionality from main method in advance of turning WebServer into usable class for other systems rather than as a program in its own right (i.e. with a main method).

--- src/main/java/com/jweb/server/WebServer.java
+++ src/main/java/com/jweb/server/WebServer.java
@@ -3,25 +3,61 @@ package com.jweb.server;
 import com.jweb.ai.ChatHandler;
 import com.jweb.ai.MicrobotHandler;
 import com.jweb.todolist.ToDoHandler;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import org.apache.commons.cli.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
-public class WebServer {
+public class WebServer
+{
+    HttpServer server;
+
     // Main Method
     public static void main(String[] args) throws IOException
     {
+        HashMap<String, HttpHandler> handlers = new HashMap<>();
+        handlers.put("/chat/", new ChatHandler());
+        handlers.put("/todolist/", new ToDoHandler());
+        handlers.put("/microbot/", new MicrobotHandler());
+        handlers.put("/test/", new TestHandler());
+        handlers.put("/", new HTMLHandler());
+
+        WebServer server = new WebServer(parseArgsToAddress(args), handlers);
+        server.start();
+    }
+
+    public WebServer(String[] args, HashMap<String, HttpHandler> routes) throws IOException
+    {
+        this(parseArgsToAddress(args), routes);
+    }
+
+    public WebServer(InetSocketAddress address, HashMap<String, HttpHandler> routes) throws IOException
+    {
+        // Create the server to listen on the specified socket address
+        server = HttpServer.create(address, 0);
+
+        // Create a context for each of the specified routes
+        for (Map.Entry<String, HttpHandler> entry : routes.entrySet())
+        {
+            addRoute(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public static InetSocketAddress parseArgsToAddress(String[] args)
+    {
         Options options = new Options();
 
-        Option input = new Option("ip", "ipaddress", true, "ip address of this server");
-        input.setRequired(false);
-        options.addOption(input);
+        Option ipOption = new Option("ip", "ipaddress", true, "ip address of this server");
+        ipOption.setRequired(false);
+        options.addOption(ipOption);
 
-        Option output = new Option("p", "port", true, "port on which the server will listen");
-        output.setRequired(false);
-        options.addOption(output);
+        Option portOption = new Option("p", "port", true, "port on which the server will listen");
+        portOption.setRequired(false);
+        options.addOption(portOption);
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -45,20 +81,24 @@ public class WebServer {
         else
             port = Integer.parseInt(portStr);
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
-
-        // Create a context for a specific path and set the handler
-        server.createContext("/todolist/", new ToDoHandler());
-        server.createContext("/test/", new TestHandler());
-        server.createContext("/chat/", new ChatHandler());
-        server.createContext("/microbot/", new MicrobotHandler());
-        server.createContext("/", new HTMLHandler());
-
-        // Start the server
-        server.setExecutor(null); // Use the default executor
-        server.start();
-
-        System.out.println("Server is running on " + ip + ":" + port);
+        return new InetSocketAddress(ip, port);
     }
 
+    // Start the server
+    public void start()
+    {
+        server.setExecutor(null); // Use the default executor
+        server.start();
+        System.out.println("Server is running on " + server.getAddress());
+    }
+
+    public void stop()
+    {
+        server.stop(0);
+    }
+
+    public void addRoute(String path, HttpHandler handler)
+    {
+        server.createContext(path, handler);
+    }
 }
